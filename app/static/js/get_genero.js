2ae47cf990e233dca83a6920593726e63b06a0dc
Alan Santos <alanssantos32@gmail.com>
2019-11-17 10:41:50 -0300

    Atualizado

--- app/static/js/get_genero.js
+++ app/static/js/get_genero.js
@@ -1,7 +1,6 @@
 const divGeneros = document.querySelector('#generos');
 const add = document.querySelector('#add');
 let genName = ['Ação', 'Aventura', 'Comédia', 'Documentário', 'Drama', 'Ficção científica', 'Romance', 'Suspense', 'Terror'];
-let genValue = ['action', 'aventure', 'comedy', 'documentary', 'drama', 'fiction', 'romance', 'thriller', 'horror'];
 let select, opt;
 
 add.addEventListener('click', function (e) {
@@ -9,10 +8,10 @@ add.addEventListener('click', function (e) {
     select.setAttribute('class', 'form-control col-md-2 mb-2');
     select.setAttribute('name', 'genero');
 
-    for (let i = 0; i < genValue.length; i++) {
+    for (let i = 0; i < genName.length; i++) {
         opt = document.createElement('option');
         opt.innerText = genName[i];
-        opt.value = genValue[i];
+        opt.value = genName[i];
 
         select.appendChild(opt);
     }
